156407196ebf0fd9543d5a24b664cbc96ca21671
AnkitSingh60 <ankitanwar730@gmail.com>
2022-04-24 02:00:53 +0530

    Make Name capital

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({image, name, type}) => {
     const style = type + " pokeMapDiv";
   return (
     <div className={style}>
-    <h3>{name}</h3>
+    <h3>{name.toUpperCase()}</h3>
     <img src={image} alt="pokemon" />
     <div>
       <h5>Type: {type}</h5>
